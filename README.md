d5bb59b2594f581416a2bac6aad7f2355b283c25
LordKamaYT <steamnitz@gmail.com>
2023-02-11 17:03:31 +0100

    Automatic commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 "# arzeroxjs-npm"
 
 ArzeroxJS is an NPM module using the discord.js library which allows you to create discord bots easier. It can be used anywhere and requires as only download `npm i arzeroxjs`
+
+# Version 1.1.0
+
+- Added text handler ( currently undergoing tests. )
+- Added text command maker ( undergoing tests. )
--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var client = new discord.Client({
   intents: [Guilds, GuildMembers, GuildMessages, MessageContent],
   partials: [User, Message, GuildMember, ThreadMember],
 });
+var { readdirSync } = require("fs");
 
 client.config = require("./config.json");
 client.events = new Collection();
@@ -42,5 +43,66 @@ function setStatus(text, type) {
   });
 }
 
-function textHandler(folder, subfolder) {}
+function textHandler(cmdfolder, PREFIX) {
+  const commandFolders = readdirSync(`./${cmdfolder}/`);
+  for (const folder of commandFolders) {
+    const commands = readdirSync(`./${cmdfolder}/${folder}`).filter((files) =>
+      files.endsWith(".js")
+    );
+    for (const file of commands) {
+      const command = require(`./${cmdfolder}/${folder}/${file}`);
+      client.commands.set(command.name, command);
+    }
+  }
+
+  const {
+    Client,
+    Message,
+    MessageEmbed,
+    Collection,
+    PermissionFlagsBits,
+  } = require("discord.js");
+
+  module.exports = {
+    name: "messageCreate",
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     */
+    async execute(message, client, Discord) {
+      if (!message.content.startsWith(PREFIX) || message.author.bot) {
+        return;
+      }
+      const args = message.content.slice(PREFIX.length).trim().split(/ + /);
+      const commandName = args.shift().toLowerCase();
+      const command =
+        client.commands.get(commandName) ||
+        client.commands.find(
+          (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+        );
+      if (!commandName) return;
+      if (!command) return;
+
+      command.execute(message, args, commandName, client, Discord);
+    },
+  };
+}
+
+class textCommand {
+  constructor({ name: name, code: { code } }) {
+    module.exports = {
+      name: this.name,
+      /**
+       *
+       * @param {Message} message
+       * @param {*} args
+       * @param {Client} client
+       */
+      execute(message, args, commandName, client, Discord) {
+        this.code;
+      },
+    };
+  }
+}
+
 module.exports = (botLogin, setStatus, textHandler);
