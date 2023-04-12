dfa1ecd81a24a2c78fc7db7a799a8a02d715e611
LordKamaYT <steamnitz@gmail.com>
2023-04-12 17:10:11 +0200

    Version Patch

---
+++ commit.bat
@@ -0,0 +1,5 @@
+color c
+echo Dev by Kama
+git add *
+git commit -m "Version Update"
+git push origin main
---
+++ index.js
@@ -0,0 +1,58 @@
+var discord = require("discord.js");
+var {
+  Client,
+  ActivityType,
+  Partials,
+  GatewayIntentBits,
+  Collection,
+} = require("discord.js");
+var { Guilds, GuildMembers, GuildMessages, MessageContent } = GatewayIntentBits;
+var { User, Message, GuildMember, ThreadMember } = Partials;
+var client = new discord.Client({
+  intents: [Guilds, GuildMembers, GuildMessages, MessageContent],
+  partials: [User, Message, GuildMember, ThreadMember],
+});
+const chalk = require("chalk");
+client.events = new Collection();
+client.commands = new Collection();
+client.subCommands = new Collection();
+client.guildConfig = new Collection();
+client.aliases = new Collection();
+client.userSettings = new Collection();
+client.maintenanced = false;
+
+function botLogin(token) {
+  if (!token) {
+    return console.log(chalk.redBright("ERR!"), " No Token Specified");
+  } else {
+    try {
+      client.login(token);
+      console.log(
+        `Dev by Kama\n\nLogged into ${client.user.tag}\n${client.user.username}'s ID is ${client.user.id}\nChange options in ./config.json\n${client.user.username} is now online.\nSuccesfully reloaded`
+      );
+    } catch (err) {
+      console.log(chalk.redBright("ERR!"), "Not a valid token");
+    }
+  }
+}
+
+class clientActivity {
+  constructor(type, txt, status) {
+    this.type = type;
+    this.txt = txt;
+    this.status = status;
+  }
+
+  set() {
+    client.user.setPresence({
+      activities: [
+        {
+          name: this.txt,
+          type: this.type,
+        },
+      ],
+      status: this.status,
+    });
+  }
+}
+module.exports = { botLogin, clientActivity };
