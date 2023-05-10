c8f2421f748cbedd80d5e70f880f7529c81cc67a
LordKamaYT <steamnitz@gmail.com>
2023-05-10 17:56:27 +0200

    Version Update

--- Examples/latest.js
+++ Examples/latest.js
@@ -3,4 +3,4 @@ const { botLogin, clientActivity } = require("arzeroxjs");
 botLogin("TOKEN");
 clientActivity("type", "text", "status");
 
-console.log("This is test code following the Version 1.4.0 guidelines");
+console.log("This is test code following the Version 1.8.0 guidelines");
