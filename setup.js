23d0690622c5accb7b833eb9113fbf0bca1d0071
LordKamaYT <steamnitz@gmail.com>
2023-07-03 18:09:08 +0200

    Version Update

--- setup.js
+++ setup.js
@@ -72,7 +72,7 @@ async function startCLI() {
           clearInterval(loadingInterval);
 
           fs.writeFileSync(indexFilePath, sampleCode);
-          console.log(chalk.bgBlue("\nUpdated index.js file."));
+          console.log(chalk.bgGreen("\nUpdated index.js file."));
         }
 
         await sleep(1000); // Simulate a 1-second loading time
