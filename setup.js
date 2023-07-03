7077a125d52635ea65bf348f6305dc5daa1cfc61
LordKamaYT <steamnitz@gmail.com>
2023-07-03 13:05:19 +0200

    Version Update

--- setup.js
+++ setup.js
@@ -2,36 +2,34 @@
 
 import inquirer from "inquirer";
 
-async function startCLI() {
-  console.log("📦 Activated");
+import select, { Separator } from "@inquirer/select";
 
-  const answer = await inquirer.select({
-    message: "What should ArzeroxJS Runtime Perform",
-    choices: [
-      {
-        name: "npm",
-        value: "npm",
-        description: "npm is the most popular package manager",
-      },
-      {
-        name: "yarn",
-        value: "yarn",
-        description: "yarn is an awesome package manager",
-      },
-      new Separator(),
-      {
-        name: "jspm",
-        value: "jspm",
-        disabled: true,
-      },
-      {
-        name: "pnpm",
-        value: "pnpm",
-        disabled: "(pnpm is not available)",
-      },
-    ],
-  });
-}
+const answer = await select({
+  message: "Select a package manager",
+  choices: [
+    {
+      name: "npm",
+      value: "npm",
+      description: "npm is the most popular package manager",
+    },
+    {
+      name: "yarn",
+      value: "yarn",
+      description: "yarn is an awesome package manager",
+    },
+    new Separator(),
+    {
+      name: "jspm",
+      value: "jspm",
+      disabled: true,
+    },
+    {
+      name: "pnpm",
+      value: "pnpm",
+      disabled: "(pnpm is not available)",
+    },
+  ],
+});
 
 if (process.argv[2] === "arzeroxjs") {
   startCLI();
