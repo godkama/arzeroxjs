83752a77d18756c14a36c93375cb61d53d01218f
LordKamaYT <steamnitz@gmail.com>
2023-07-03 13:15:01 +0200

    Version Update

--- setup.js
+++ setup.js
@@ -1,7 +1,32 @@
 #!/usr/bin/env node
 
+const inquirer = require("inquirer");
+
 function startCLI() {
   console.log("Initialized 📦");
+
+  const choices = [
+    { name: "Option 1", value: "option1" },
+    { name: "Option 2", value: "option2" },
+    { name: "Option 3", value: "option3" },
+  ];
+
+  const selectPrompt = {
+    type: "list",
+    name: "selectedOption",
+    message: "Select an option:",
+    choices: choices,
+  };
+
+  inquirer.prompt(selectPrompt).then((answers) => {
+    const selectedOption = answers.selectedOption;
+
+    // Handle the selected option here
+    console.log(`Selected option: ${selectedOption}`);
+
+    // Call startCLI again to continue the CLI flow
+    startCLI();
+  });
 }
 
 startCLI();
