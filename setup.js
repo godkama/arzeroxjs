c320a5a234b491cc4aed78897292dd22d5df0b12
LordKamaYT <steamnitz@gmail.com>
2023-07-03 13:04:06 +0200

    Version Update

--- setup.js
+++ setup.js
@@ -2,8 +2,35 @@
 
 import inquirer from "inquirer";
 
-function startCLI() {
+async function startCLI() {
   console.log("📦 Activated");
+
+  const answer = await inquirer.select({
+    message: "Select a package manager",
+    choices: [
+      {
+        name: "npm",
+        value: "npm",
+        description: "npm is the most popular package manager",
+      },
+      {
+        name: "yarn",
+        value: "yarn",
+        description: "yarn is an awesome package manager",
+      },
+      new Separator(),
+      {
+        name: "jspm",
+        value: "jspm",
+        disabled: true,
+      },
+      {
+        name: "pnpm",
+        value: "pnpm",
+        disabled: "(pnpm is not available)",
+      },
+    ],
+  });
 }
 
 if (process.argv[2] === "arzeroxjs") {
