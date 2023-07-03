4358cc7d1fecb223dc8be8901f7d12a3431c2e0c
LordKamaYT <steamnitz@gmail.com>
2023-07-03 13:13:42 +0200

    Version Update

--- setup.js
+++ setup.js
@@ -1,35 +1,7 @@
 #!/usr/bin/env node
 
-const select = require("@inquirer/select");
-const { Separator } = require("@inquirer/select");
-
 async function startCLI() {
-  const answer = await select({
-    message: "Select a package manager",
-    choices: [
-      {
-        name: "npm",
-        value: "npm",
-        description: "npm is the most popular package manager",
-      },
-      {
-        name: "yarn",
-        value: "yarn",
-        description: "yarn is an awesome package manager",
-      },
-      new Separator(),
-      {
-        name: "jspm",
-        value: "jspm",
-        disabled: true,
-      },
-      {
-        name: "pnpm",
-        value: "pnpm",
-        disabled: "(pnpm is not available)",
-      },
-    ],
-  });
+  console.log("Initialized 📦");
 }
 
 startCLI();
