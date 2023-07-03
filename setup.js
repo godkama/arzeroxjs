0cd4f59ccb961fc39b637d62420961673152d5b4
LordKamaYT <steamnitz@gmail.com>
2023-07-03 13:09:17 +0200

    Version Update

--- setup.js
+++ setup.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { select, Separator } = require("@inquirer/select");
+const { select, Separator } = require("inquirer");
 
 async function startCLI() {
   const answer = await select({
