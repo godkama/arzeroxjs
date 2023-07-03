844ddb694f105be21b22aa9e42b8f4431ec25f48
LordKamaYT <steamnitz@gmail.com>
2023-07-03 13:08:58 +0200

    Version Update

--- setup.js
+++ setup.js
@@ -2,33 +2,34 @@
 
 const { select, Separator } = require("@inquirer/select");
 
-const answer = await select({
-  message: "Select a package manager",
-  choices: [
-    {
-      name: "npm",
-      value: "npm",
-      description: "npm is the most popular package manager",
-    },
-    {
-      name: "yarn",
-      value: "yarn",
-      description: "yarn is an awesome package manager",
-    },
-    new Separator(),
-    {
-      name: "jspm",
-      value: "jspm",
-      disabled: true,
-    },
-    {
-      name: "pnpm",
-      value: "pnpm",
-      disabled: "(pnpm is not available)",
-    },
-  ],
-});
-
+async function startCLI() {
+  const answer = await select({
+    message: "Select a package manager",
+    choices: [
+      {
+        name: "npm",
+        value: "npm",
+        description: "npm is the most popular package manager",
+      },
+      {
+        name: "yarn",
+        value: "yarn",
+        description: "yarn is an awesome package manager",
+      },
+      new Separator(),
+      {
+        name: "jspm",
+        value: "jspm",
+        disabled: true,
+      },
+      {
+        name: "pnpm",
+        value: "pnpm",
+        disabled: "(pnpm is not available)",
+      },
+    ],
+  });
+}
 if (process.argv[2] === "arzeroxjs") {
   startCLI();
 }
