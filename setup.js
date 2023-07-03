89e915ff948249e232e2d27e0d9382487fe03516
LordKamaYT <steamnitz@gmail.com>
2023-07-03 13:56:52 +0200

    Version Update

--- setup.js
+++ setup.js
@@ -5,7 +5,7 @@ const { clientLogin, clientStatus } = require("./index.js");
 const inquirer = require("inquirer");
 const password = require("inquirer");
 
-async function startCLI() {
+function startCLI() {
   const choices = [
     { name: "Initialize", value: "init" },
     { name: "Login", value: "login" },
@@ -23,23 +23,24 @@ async function startCLI() {
     const selectedOption = answers.selectedOption;
 
     // Handle the selected option here
-  });
-  switch (selectedOption) {
-    case "init":
-      console.log(chalk.greenBright("Initialized 📦"));
-      break;
-    case "login":
-      const { token } = await inquirer.prompt({
-        type: "password",
-        name: "password",
-        message: "Enter your password:",
-        mask: "*", // Mask the input with asterisks
-      });
+    switch (selectedOption) {
+      case "init":
+        console.log(chalk.greenBright("Initialized 📦"));
+        break;
+      case "login":
+        const { token } = await inquirer.prompt({
+          type: "password",
+          name: "password",
+          message: "Enter your password:",
+          mask: "*", // Mask the input with asterisks
+        });
 
-      clientLogin(token);
-    case "exit":
-      process.exitCode("Exiting...");
-  }
+        clientLogin(token);
+      case "exit":
+        process.exitCode("Exiting...");
+    }
+
+  });
 }
 
 startCLI();
