cf65259b4fefb21de5f72f863c841d1b607d4abd
LordKamaYT <steamnitz@gmail.com>
2023-07-03 17:50:09 +0200

    Version Update

--- setup.js
+++ setup.js
@@ -65,7 +65,7 @@ async function startCLI() {
           console.error(`Error opening index.js for editing: ${error}`);
         }
       });
-
+      break;
     case "login":
       const { token } = await inquirer.prompt({
         type: "password",
